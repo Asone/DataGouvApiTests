81dd8809e00b78f984b1ef2bbaeab48860494e78
Asone <asone@akbarworld.info>
2014-09-10 23:08:57 +0200

    class for scrapping files

---
+++ scrap_files.rb
@@ -0,0 +1,23 @@
+require 'open-uri'
+require 'json'
+require 'yaml'
+require 'fileutils'
+require 'curb'
+
+class files_scrapper
+	
+	def initialize(*ARGS)
+		@format_filter =
+	end
+
+	def download_files
+	
+	
+	
+	
+	end
+	
+	def run 
+	
+	end
+end
