6beb748dc461f4ba1bfbf7ab44d75d0177548695
Asone <asone@akbarworld.info>
2014-09-15 13:49:40 +0200

    filter zip and API also, just in case

--- scrap_files.rb
+++ scrap_files.rb
@@ -24,40 +24,45 @@ class Files_Scrapper
 	end
 
 	def download_files()
+		c = 0
 		dirs = Dir[@root_data_dir+'*'].map { |a| File.basename(a) }
-		dirs.each do |d|
-			files =	Dir[@root_data_dir+d+'/*.json']
-			files.each do |f|
-				 File.open(f, "r" ) do |ds|
-					 dataset = JSON.load( ds )
-					 
-					  if(dataset['resources'])
-					  case @status_filter
-					  when 'all'
-					  
-					 # p 'process all formats files scrapping...'
-					  when 'open'
-					  
-					  p 'process open format files scrapping...'
-					  when 'crappy'
-					  has_proprietary_format = dataset['resources'].find_index{|r| r['filetype'] == ('XLS'||'xls') }
-					  has_open_format = dataset['resources'].find_index{|r| r['filetype'] == ('csv'||'CSV'||'json'||'JSON'||'xml'||'XML')}
-					 # puts has_format
-					  if(has_proprietary_format != nil)&&(has_open_format == nil)
-					 	p dataset['resources'][has_proprietary_format]['url']
-					  end
-					 if (dataset['resources'].length > 0)
-					#	p dataset['resources'].find_index{|r| r['filetype'] == 'CSV'||'csv' } 
-						if Dir.exists?(@root_files_dir+d.to_s) != true
-							 #	FileUtils::mkdir_p @data_dir+o.to_s
-						end
+			dirs.each do |d|
+				files =	Dir[@root_data_dir+d+'/*.json']
+				files.each do |f|
+					 File.open(f, "r" ) do |ds|
+						 dataset = JSON.load( ds )
+						 
+						  if(dataset['resources'])
+						  case @status_filter
+						  when 'all'
+						  
+						 # p 'process all formats files scrapping...'
+						  when 'open'
+						  
+						  p 'process open format files scrapping...'
+						  when 'crappy'
+						  has_proprietary_format = dataset['resources'].find_index{|r| r['filetype'] == ('XLS'||'xls') }
+						  has_open_format = dataset['resources'].find_index{|r| r['filetype'] == ('csv'||'CSV'||'json'||'JSON'||'xml'||'XML'||'zip'||'ZIP'||'api'||'API')}
+						 # puts has_format
+						  if(has_proprietary_format != nil)&&(has_open_format == nil)
+						  c = c+1
+						 	p dataset['resources'][has_proprietary_format]['url']
+						 	puts 'le compteur indique :'+ c.to_s
+						  end
+						 if (dataset['resources'].length > 0)
+						#	p dataset['resources'].find_index{|r| r['filetype'] == 'CSV'||'csv' } 
+							if Dir.exists?(@root_files_dir+d.to_s) != true
+								 #	FileUtils::mkdir_p @data_dir+o.to_s
+							end
+						 end
 					 end
-				 end
+				end
 			end
 		end
+	
 	end
 	end
-	end
+	
 	def run
 		download_files()
 	end
