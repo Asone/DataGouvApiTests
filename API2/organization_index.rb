bbb8fcaf80aadf47a65cf112bb6ea293d99febfc
Asone <asone@akbarworld.info>
2014-09-21 15:12:23 +0200

    script seems to work

--- API2/organization_index.rb
+++ API2/organization_index.rb
@@ -12,7 +12,7 @@ class DataGouv_Orgnizations_db
 		  @base_url = "http://www.data.gouv.fr/api/3/action/"
 	    @base_url_qa = "http://qa.data.gouv.fr/api/1/"
 	    @public_url = "http://www.data.gouv.fr/fr/"
-	    @data_dir = File.dirname(__FILE__)+'../data/'
+	    @data_dir = File.dirname(__FILE__)+'../dataAPI2/'
 	    @files_dir = File.dirname(__FILE__)+'../files/'
 	    
 	end
@@ -30,58 +30,48 @@ class DataGouv_Orgnizations_db
   			orga_metadata = JSON.parse(Curl.get(orga_url).body_str)
  			orga_datasets = orga_metadata['value']['packages']
  			 			
- 		#	 orga_metadata['value']['name']
- 			# p orga_datasets
+ 	
  			datasets =  []
  			
  			# build orgnizations folders 
  			
  			if Dir.exists?(@data_dir+orga_metadata['value']['name'].to_s) != true
- 				FileUtils::mkdir_p @data_dir+o.to_s
+ 				FileUtils::mkdir_p @data_dir+orga_metadata['value']['name'].to_s
  			end
  			
  			orga_index = {'org' => orga_metadata['value']['name'].to_s, 'packages' => [] }
- #			
+
      		if orga_datasets != nil
 	 			orga_datasets.each do |pack|
-	 		#	 p pack
+		 			
+		 			resources = [] 
 	 				orga_index['packages'].push(pack['name'])
 	 				
-	 				#p pack['id']
 	 				
 	 				url = @base_url_qa+'datasets/'+pack['id']
-	 			#	p url
 	 				package_metadata = JSON.parse(Curl.get(url).body_str)
-	 				if !package_metadata['error']
-	 				p package_metadata['value']['resources']
-	 				end
-	 			#		p package_metadata
-	 			#	if package_metadata['value']['resources'] != nil
-	 			#		p package_metadata['value']['resources']
-	 			#	end			
-	 #				resources = [] 
-	 #				
-	 #				p pack['name']
-	 #				# @data_dir+o.to_s+'/'+pack['name']+'.json'
-	 #				#p package_metadata['result']
-	 #				package_metadata['result']['resources'].each do |r|
-	 #				p r['url']
-	 #					resources.push({'url' => r['url'],'filetype' => r['format'],'name' => r['name'] })
-	 #					# p  'org : '+o.to_s+' - pack : '+ pack['name']+'  - rsc : '+r['format'].to_s+' count : '+c.to_s
-	 #				end
-	 #				
-	 #				dataset = { 'name' => pack['name'], 'resources' => resources }
-	 #				datasets.push(dataset)
-	 #				File.open(@data_dir+o.to_s+'/'+pack['name']+'.json', 'w') {|f| f.write dataset.to_json }
-	 #				sleep(1.0/3.0)
-	 	
+	 			
+		 				if !package_metadata['error']
+		 					p package_metadata['value']['num_resources']
+							if package_metadata['value']['num_resources'].to_i > 0		 					
+				 				package_metadata['value']['resources'].each do |r|
+					 			
+					 				resources.push({'url' => r['url'],'filetype' => r['format'],'name' => r['name'] })
+					 				
+				 				end
+				 			dataset = { 'name' => pack['name'], 'resources' => resources }
+				 			datasets.push(dataset)
+				 			File.open(@data_dir+orga_metadata['value']['name'].to_s+'/'+pack['name']+'.json', 'w') {|f| f.write dataset.to_json }
+				 			end
+			 			#		resources.push({'url' => ['url'],'filetype' => r['format'],'name' => r['name'] })
+			 					file_url = @data_dir+orga_metadata['value']['name'].to_s+'/'+package_metadata['value']['name']+'.json'
+			 			end
+			 			
+		 			
+			 	end
+			 	File.open(@data_dir+orga_metadata['value']['name'].to_s+'/index.json','w'){|f| f.write orga_index.to_json }
+			 #	File.open(@data_dir+o.to_s+'/index.json','w'){|f| f.write orga_index.to_json }
  			end
- #				organization = {'name' => o, 'datasets' => datasets }
- #				organizations_full.push(organization)
- #				organization_base_set.push('name' => o.to_s, 'length' => organization.length )
- #				File.open(@data_dir+o.to_s+'/index.json','w'){|f| f.write orga_index.to_json }
- 			end
- #			File.open(@data_dir+'/organizations.json','w'){|f| f.write organization_base_set.to_json }
  	end
  	end
 	
