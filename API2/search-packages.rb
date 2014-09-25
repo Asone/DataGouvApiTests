528aa6691c0ed2f520e098eadc14f08e41220f95
Asone <asone@akbarworld.info>
2014-09-25 16:38:59 +0200

    prepare class for searching and generating outputs

---
+++ API2/search-packages.rb
@@ -0,0 +1,23 @@
+require 'open-uri'
+require 'json'
+require 'yaml'
+require 'fileutils'
+require 'curb'
+
+Conf = YAML.load_file("../config.yml")
+
+	class datagouvsearch
+	
+	def initialize(*args)
+	end
+	
+	def dispatcher
+	end
+	
+	def generate_output
+	end
+	
+	def run
+	end
+
+end
